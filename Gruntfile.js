956a16af5e5751614ae363f8cdc97eefe724e669
tyleralmond <tylera@careercruising.com>
2017-12-04 21:23:39 -0500

    Update packages and serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,8 @@ module.exports = function (grunt) {
         dist: 'dist'
     };
 
+    var serveStatic = require('serve-static');
+
     // Define the configuration for all the tasks
     grunt.initConfig({
 
@@ -80,9 +82,9 @@ module.exports = function (grunt) {
                 options: {
                     middleware: function(connect) {
                         return [
-                            connect.static('.tmp'),
-                            connect().use('/bower_components', connect.static('./bower_components')),
-                            connect.static(config.app)
+                            serveStatic('.tmp'),
+                            connect().use('/bower_components', serveStatic('./bower_components')),
+                            serveStatic(config.app)
                         ];
                     }
                 }
@@ -93,10 +95,10 @@ module.exports = function (grunt) {
                     port: 9001,
                     middleware: function(connect) {
                         return [
-                            connect.static('.tmp'),
-                            connect.static('test'),
-                            connect().use('/bower_components', connect.static('./bower_components')),
-                            connect.static(config.app)
+                            serveStatic('.tmp'),
+                            serveStatic('test'),
+                            connect().use('/bower_components', serveStatic('./bower_components')),
+                            serveStatic(config.app)
                         ];
                     }
                 }
